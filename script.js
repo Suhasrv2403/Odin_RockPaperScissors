0b5fb2232b0ccc9bcd5653de137fbaf0a4060eee
Suhas R Vittal <suhasrvittal24032000@gmail.com>
2023-02-28 23:28:51 +0530

    Files added

---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+
+
+alert(playRound(playerSelection , computerSelection));
+
+function getComputerChoice(){
+const selector = ["rock","paper","scissor"];
+var a = (Math.floor(Math.random()*10) %3);
+return selector[a];
+}
+
+
+
+function playRound(playerSelection , computerSelection){
+
+switch(playerSelection){
+	case "rock":
+		switch(computerSelection){
+			case "rock":
+				return ("Tie! Both choose rock");
+				brea;
+			case "paper":
+				return ("You loose! Paper beats Rock");
+				break;
+			case "scissor":
+				return ("You Win! Rock beats scissor");
+				break;
+			default:
+				return("Invalid choice");
+				break;
+					}
+			break;
+	case "paper":
+			switch(computerSelection){
+			case "rock":
+				return ("You Win! paper beats rock");
+				brea;
+			case "paper":
+				return ("Tie! Both choose paper");
+				break;
+			case "scissor":
+				return ("You loose! scissor beats paper");
+				break;
+			default:
+				return("Invalid choice");
+				break;
+						}
+				break;
+				
+	case "scissor" :
+	
+			switch(computerSelection){
+			case "rock":
+				return ("You loose! rock beats scissor");
+				brea;
+			case "paper":
+				return ("You win! scissor beats paper");
+				break;
+			case "scissor":
+				return ("Tie! Both choose scissor");
+				break;
+			default:
+				return("Invalid choice");
+				break;
+						}
+				break;
+			
+		default:
+			return("Wrong choice");
+			break;
+			
+					}
+	}
+						
+
